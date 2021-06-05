5d7e83ab030b5871f7441ec7e4d3df17fd5312c5
Jackson Cai <jcai8649@gmail.com>
2021-06-05 12:39:11 -0800

    Add user login validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,6 @@
+import bcrypt from "bcrypt";
 import { Request, Response, Router } from "express";
-import { validate } from "class-validator";
+import { validate, isEmpty } from "class-validator";
 
 import { User } from "../entities/User";
 
@@ -35,7 +36,33 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = "Username must not be empty";
+    if (isEmpty(password)) errors.password = "Password must not be empty";
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(401).json({ password: "Password is incorrect" });
+    }
+
+    return res.json(user);
+  } catch (err) {}
+};
+
 const router = Router();
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
